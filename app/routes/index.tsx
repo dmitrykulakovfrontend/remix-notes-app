c28cf89edb93465acedc87c0fca0e9001684e631
Dmitry Kulakov <atomeistee@gmail.com>
2022-08-15 15:38:27 +0300

    install tailwind and packages for it
    installed and setup tailwindcss, autoprefixer, concurrently, postcss

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
     </div>
   );
 }
