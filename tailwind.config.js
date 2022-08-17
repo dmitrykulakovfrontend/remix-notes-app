f20e6bf1e6b48a9ac27fb8e400abcd3600d4fe3f
Dmitry Kulakov <atomeistee@gmail.com>
2022-08-17 13:48:52 +0300

    Add custom colors from figma file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      'lightPrimary': '#EEEEEE',
+      'lightSecondary': '#D6E3E2',
+      'darkPrimary': '#3A3F47',
+      'lightPlaceholder': '#638787',
+      'redAccent': '#D65B32',
+      'greenAccent': '#8B9A47',
+    }
   },
   plugins: [],
 }
